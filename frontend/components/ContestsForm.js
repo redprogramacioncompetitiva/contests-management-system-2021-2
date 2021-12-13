a9d4cd2ce3cd9a57f5915d30e436edefb2aca364
CPASUY <carolinapasuy@hotmail.com>
2021-12-13 11:12:12 -0500

    Implementacion de los botones de navegavilidad

--- frontend/components/ContestsForm.js
+++ frontend/components/ContestsForm.js
@@ -28,12 +28,26 @@ class ContestsForm extends React.Component {
 
                 <center><a href="https://redprogramacioncompetitiva.com/"><img src="https://pbs.twimg.com/profile_images/493847405670850561/qslkfHlq_400x400.jpeg" alt="RPC_Logo" width="100" height="100" /></a></center>
 
-<center><h1 className="display-2">RPC :: Red de Programación Competitiva</h1></center>
+                <center><h1 className="display-2">RPC :: Red de Programación Competitiva</h1></center>
                 </div>
 
                 <br />
+                <div className="py-1 text-center">
+                <Link href="http://localhost:3000/createContest">
 
-                
+                    <a className="btn btn-primary" role="button">Create Contest</a>
+
+                </Link>
+                </div>  
+                <br>
+                </br>
+                <div className="py-1 text-center">
+                <Link href="http://localhost:3000/registerInCompetition">
+
+                    <a className="btn btn-primary" role="button">Register a Contest</a>
+
+                </Link>
+                </div>  
 
                 <form className="w-50 mx-auto p-2" method="GET" action = "http://localhost:8080/contests">
                 
